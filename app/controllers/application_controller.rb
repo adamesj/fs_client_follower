cbc60a208d11c6945eff4376e8ee8e00814c9a49
Jimmy Adames <jimmyadames@Armisteads-iMac.local>
2017-04-07 18:06:32 -0400

    Retrieved pictures for all clients

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+  	def all_clients
+  		@client = Client.all
+  	end
+
   	def current_user
   		@current_user ||= User.find(session[:user_id]) if session[:user_id]
   	end
